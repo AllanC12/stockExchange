70184a57a68c58e8955b270d12de651b456d5ead
AllanC12 <allandevfront@gmail.com>
2023-07-29 14:24:37 -0300

    adicionando arquivos de estilo e configurando mixins

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
+
+import logo from "../../public/img/logo.png"
 
 const NavBar = () => {
+
+  const [statusUser,setStatusUser] = useState(false)
+
   return (
-    <div>NavBar</div>
+      <nav className="navbar">
+        <div className="logo">
+          <img src={logo} alt="logotipo" />
+        </div>
+        {
+          statusUser ? (
+            <p>Logado</p>
+          ) : (
+            <p>Não logado</p>
+          )
+        }
+      </nav>
   )
 }
 
