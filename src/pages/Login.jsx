cc6bd8fa394cac9c85b46811fb12ce9c89337679
AllanC12 <allandevfront@gmail.com>
2023-07-31 22:04:44 -0300

    formulário de login e estilos iniciais implementados

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import "./sass_pages/Login.scss"
 
+import Form from "../components/Form"
+ 
 const Login = () => {
   return (
     <div className="login">
-      
+      <Form/>
     </div>
   )
 }
