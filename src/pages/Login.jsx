25c35237ad24164aee53ce48f6d0737078968976
AllanC12 <allandevfront@gmail.com>
2023-08-19 12:50:53 -0300

    melhorando css de formulário

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,16 +2,13 @@ import "./sass_pages/Login.scss";
 
 import { useState } from "react";
 
-import FormRegister from "../components/FormRegister";
 import FormLogin from "../components/FormLogin";
 
 
 const Login = () => {
-  const [statusLogin] = useState(true);
-  
   return (
     <div className="login">
-      {statusLogin ? <FormLogin /> : <FormRegister />}
+      <FormLogin />
     </div>
   );
 };
