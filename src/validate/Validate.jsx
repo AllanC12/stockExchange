de85ac70791c089601fc9f750915b0655133812b
AllanC12 <allandevfront@gmail.com>
2023-09-03 22:55:15 -0300

    buscando por id de usuário

---
+++ src/validate/Validate.jsx
@@ -0,0 +1,52 @@
+import {MyContextDataUser} from "../context/ContextDataUser"
+
+export const verifyUserRegister = async (dataUser, setMessage) => {
+
+  const emailUser = dataUser.email;
+  const urlEmail = `${import.meta.env.VITE_URL_EMAIL}=${emailUser}`;
+  const respRegister = await fetch(urlEmail).then((resp) => resp.json());
+  const pageRegister = "http://localhost:5173/register";
+  const pageLogin = "http://localhost:5173/";
+
+  const {id} = respRegister[0]
+  
+  const setUserId = MyContextDataUser()
+
+  if (location.href === pageRegister) {
+    if (respRegister.length > 0) {
+      return true;
+    }
+  }
+
+  if (location.href === pageLogin) {
+    if (respRegister.length === 0) {
+      setMessage("Verifique seu email");
+      return true;
+    }
+  }
+
+  if (respRegister.length === 0) {
+    return;
+  } else {
+    if (respRegister[0].password === dataUser.password) {
+      setMessage(`Olá ${respRegister[0].name}`);
+    } else {
+      setMessage("Verifique sua senha");
+      return true;
+    }
+  }
+};
+
+export const validateUserRegister = (dataUser) => {
+  const keys = Object.keys(dataUser);
+
+  for (let i = 0; i < keys.length; i++) {
+    if (dataUser[keys[i]] === "") {
+      return false;
+    } else if (dataUser.password !== dataUser.confirmPassword) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
